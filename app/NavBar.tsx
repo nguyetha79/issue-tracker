aeae818e0dbea3acc240753bedc950e3cb58ae72
nguyetha79 <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-07-01 19:20:28 +0200

    add active links in the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,13 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
-type Props = {};
+const NavBar = () => {
+  const currentPath = usePathname();
 
-const NavBar = (props: Props) => {
   const links = [
     {
       label: "Dashboard",
@@ -25,7 +28,11 @@ const NavBar = (props: Props) => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
