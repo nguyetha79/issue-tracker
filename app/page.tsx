33c0b6c98366af930933c79e4ac32926070a1273
nguyetha79 <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-07-01 20:07:57 +0200

    set up Radix UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@radix-ui/themes'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div><button>New issue</button></div>
   )
 }
