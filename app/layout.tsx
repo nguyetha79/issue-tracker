e9c27c574fc724536eb569628ec91d58d2cf0d0d
nguyetha79 <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-07-11 13:09:09 +0200

    add the login and logout links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
-import "./theme-config.css";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AuthProvider from "./auth/Provider";
+import "./globals.css";
 import NavBar from "./NavBar";
-import { Container, Theme, ThemePanel } from "@radix-ui/themes";
+import "./theme-config.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -21,12 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="violet">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
