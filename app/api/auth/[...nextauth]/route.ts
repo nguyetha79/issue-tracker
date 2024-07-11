7360f358b3c8414059a2da81ba3639b06a790a08
nguyetha79 <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-07-11 19:27:43 +0200

    securing the application

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,7 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-};
+import authOptions from "@/app/auth/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
