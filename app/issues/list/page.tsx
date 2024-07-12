032246a8a55b237669ca231ef64d4ed0d9ad63d5
nguyetha79 <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2024-07-12 13:24:02 +0200

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,22 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+type Props = {
+  searchParams: { status: Status };
+};
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: status,
+    },
+  });
   return (
     <div>
       <IssueActions />
@@ -43,6 +56,6 @@ const IssuesPage = async () => {
   );
 };
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default IssuesPage;
